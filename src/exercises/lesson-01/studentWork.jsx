cbb1814268bdb3a404a0209e048460910701ffe8
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-07-14 12:22:43 -0400

    completed lesson 1 assignment

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,26 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Alix V.';
+  const age = 31;
+  const hobbies = [
+    { id: 1, title: 'read' },
+    { id: 2, title: 'bike' },
+    { id: 3, title: 'garden' },
+  ];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>{name}</h1>
+      <p>
+        {' '}
+        Hi, my name is {name} and I am {age} years old. I am an aspiring
+        software engineer, taking the Code the Dream React course. I am kind of
+        a private and boring individual, but my cat is the best thing I have
+        going on in my life, so I live for her. In my spare time, I like to:
+        {hobbies.map((hobby) => (
+          <li key={hobby.id}> {hobby.title}</li>
+        ))}
+      </p>
     </div>
   );
 }
